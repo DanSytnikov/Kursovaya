9bb9c90259af67c166cff1e3639214ca1ab0c579
DanSytnikov <passlord99>
2018-05-15 09:23:51 +0300

    1

--- app/src/main/java/com/example/student/weatherapi/MainActivity.java
+++ app/src/main/java/com/example/student/weatherapi/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.student.weatherapi;
 
 import android.nfc.Tag;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,7 +9,10 @@ import android.view.View;
 
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -29,18 +33,21 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onClick(View view){
+        /*
         ThreadWeather threadWeather = new ThreadWeather();
         threadWeather.start();
-
-
+*/
+        ThreadDownloader threadDownloader = new ThreadDownloader();
+        threadDownloader.start();
     }
 
+    /*
     public class ThreadWeather extends Thread {
         @Override
         public void run() {
             Log.e(TAG, "I'm alive");
             try {
-                URL url = new URL("http://api.openweathermap.org/data/2.5/weather?q=Odessa&appid=a7c76f80c9580699351007ff55f2e86d");
+                URL url = new URL("https://speed.hetzner.de/100MB.bin");
                 HttpURLConnection huc = (HttpURLConnection)url.openConnection();
                 BufferedReader br = new BufferedReader(new InputStreamReader(huc.getInputStream()));
                 StringBuilder sb = new StringBuilder();
@@ -55,4 +62,42 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+*/
+    public class ThreadDownloader extends Thread{
+        @Override
+        public void run(){
+            try{
+                URL url = new URL("https://speed.hetzner.de/100MB.bin");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setDoOutput(true);
+
+                urlConnection.connect();
+
+                File SDCardRoot = Environment.getExternalStorageDirectory();
+                File file =  new File(SDCardRoot, "100MB.bin");
+
+                FileOutputStream fileOutput = new FileOutputStream(file);
+                InputStream inputStream = urlConnection.getInputStream();
+
+                int totalSize = urlConnection.getContentLength();
+                int downloadSize = 0;
+                byte[] buffer = new byte[1024];
+                int bufferLength = 0;
+
+                while ((bufferLength = inputStream.read(buffer)) > 0){
+                    fileOutput.write(buffer, 0, bufferLength);
+                    downloadSize += bufferLength;
+                    //updateProgress(downloadSize, totalSize);
+
+                }
+                Log.d("TAG", "File downloaded");
+                fileOutput.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 }
