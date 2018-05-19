917e6500668a1797bc2ea291123c7693f8a875e2
Dan Sytnikov <dan.sytnikov@gmail.com>
2018-05-19 03:16:57 +0300

    1

--- app/src/main/java/com/example/student/weatherapi/MainActivity.java
+++ app/src/main/java/com/example/student/weatherapi/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.student.weatherapi;
 
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.nfc.Tag;
+import android.os.Build;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -17,6 +30,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import static java.net.Proxy.Type.HTTP;
 
@@ -24,21 +40,40 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MyActivity";
 
 
+    Handler handler;
+
+
+    Thread currentThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView tv = findViewById(R.id.status);
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                int n = (int)Math.round(100.0/(msg.arg1*1.0 /(int)(msg.obj)));
+                ProgressBar pb = findViewById(R.id.progressBar);
+                pb.setProgress(n);
+                tv.setText("" + (int)msg.obj/1048576 +"Mb\n" + n +"%");
+            }
+        };
     }
 
+    public void onClick(View view) {
+        findViewById(R.id.status).setVisibility(View.VISIBLE);
+        currentThread = new ThreadDownloader();
+        TextView sb = (TextView) findViewById(R.id.stop);
+        sb.setText("Stop");
+        currentThread.start();
+    }
 
-    public void onClick(View view){
-        /*
-        ThreadWeather threadWeather = new ThreadWeather();
-        threadWeather.start();
-*/
-        ThreadDownloader threadDownloader = new ThreadDownloader();
-        threadDownloader.start();
+    public void ClickStop(View view) {
+        currentThread.interrupt();
+        TextView sb = (TextView) findViewById(R.id.stop);
+        sb.setText("Downloading stopped");
+
     }
 
     /*
@@ -63,41 +98,65 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 */
-    public class ThreadDownloader extends Thread{
+    public class ThreadDownloader extends Thread {
         @Override
-        public void run(){
-            try{
+        public void run() {
+            super.run();
+            Log.d(TAG, "Starting thread...");
+            try {
                 URL url = new URL("https://speed.hetzner.de/100MB.bin");
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.setDoOutput(true);
+                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
 
-                urlConnection.connect();
+                InputStream inputStream = urlConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
                 File SDCardRoot = Environment.getExternalStorageDirectory();
-                File file =  new File(SDCardRoot, "100MB.bin");
+                Log.e("SD", SDCardRoot.getAbsolutePath());
+
+                File sdPath = new File(SDCardRoot.getAbsolutePath() + "/" + "Downloaded");
+                sdPath.mkdirs();
+                File file = new File(sdPath, System.currentTimeMillis() + "file");
+                Log.d("PATH", file.getAbsolutePath());
 
                 FileOutputStream fileOutput = new FileOutputStream(file);
-                InputStream inputStream = urlConnection.getInputStream();
 
-                int totalSize = urlConnection.getContentLength();
-                int downloadSize = 0;
+                urlConnection.setRequestMethod("GET");
+
+                if (urlConnection.getResponseCode() == HttpsURLConnection.HTTP_OK) {
+                    Log.d("CON", "Connection set. Code:" + HttpsURLConnection.HTTP_OK);
+                } else {
+                    Log.e("CON", "Connection error");
+                }
+
                 byte[] buffer = new byte[1024];
-                int bufferLength = 0;
+                int bufferLength;
+                int totalSize = urlConnection.getContentLength();
+                int downloadedSize = 0;
+                Log.d("FILE", "totalSize: " + totalSize);
 
-                while ((bufferLength = inputStream.read(buffer)) > 0){
+                while ((bufferLength = inputStream.read(buffer)) > 0) {
                     fileOutput.write(buffer, 0, bufferLength);
-                    downloadSize += bufferLength;
-                    //updateProgress(downloadSize, totalSize);
+                    downloadedSize += bufferLength;
+
+                    Message msg = new Message();
+                    msg.obj = downloadedSize;
+                    msg.arg1 = totalSize;
+                    handler.sendMessage(msg);
+
+                    if (isInterrupted()) {
+                        file.delete();
+                        break;
+                    }
 
                 }
+                Log.d("FILE", "DownloadedSize: " + downloadedSize);
                 Log.d("TAG", "File downloaded");
                 fileOutput.close();
             } catch (IOException e) {
                 e.printStackTrace();
+                Log.e("BLET", "Neponyatnaya hernia brat");
             }
-
         }
     }
-
 }
+
